7cf7a08a009b4ca3cb0388356c9a227fece03843
Rahul Kukreti <kukretir793@gmail.com>
2026-01-27 20:00:30 +0530

    Delete pages/chat.tsx

--- pages/chat.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const ChatClient = dynamic(() => import('../components/ChatClient'), { ssr: false });
-
-export default function ChatPage() {
-  return <ChatClient />;
-}
